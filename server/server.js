aa8d7cb2dce54a5e3bd79bca0b96495588750add
changeyu <coder.zy@qq.com>
2018-10-07 22:33:40 +0800

    del console

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
 io.on('connection',(socket)=>{
-	console.log('io connection')
+	// console.log('io connection')
 	socket.on('sendmsg',data=>{
 		io.emit('recvmsg',data)
 	})
